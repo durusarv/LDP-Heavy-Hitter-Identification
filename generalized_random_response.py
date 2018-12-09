40fb0c4ebc985fe0b421993e192ac4c5f5d43a31
jlips24 <jake_lipson@yahoo.com>
2018-12-09 16:14:01 -0500

    pi and aggregator in general_random_response.py is complete

---
+++ generalized_random_response.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+@author: durusarv, jdlips, tycorr (Duruvan Sarvanan, Jake Lipson and Tyler Correll)
+"""
+
+import numpy as np
+import xxhash
+import math
+
+class GRR:
+
+    def __init__(self, dist, epsilon, domain, data):
+
+        self.d = domain
+        self.epsilon = epsilon
+        self.p = math.exp(self.epsilon) / (math.exp(self.epsilon) + self.d - 1)
+
+        self.real = dist
+        self.estimate = np.zeros(self.d)
+
+        self.numUsers = len(data)
+        self.perturbed = np.zeros(self.numUsers)
+
+        self.randomIdxs = np.random.rand(1, self.numUsers)
+
+    def pi(self):
+        for i in range(self.numUsers):
+            coin = np.random.random()
+            if coin <= self.p:
+                self.pertubed[i] = data[i]
+            else:
+                temp = np.random.randint(0, len(data) - 1
+                if (temp == i):
+                    temp += 1
+                self.pertubed[i] = data[temp]
+
+    def aggregator(self):
+
+        for i in range(self.numUsers):
+            for j in range(self.d):
+                if self.perturbed[i] == (xxhash.xxh32(str(self.randomIdx[i]), seed=i).intdigest() % (math.exp(self.epsilon) + 1)):
+                    self.estimate[j] += 1
+        x = math.exp(self.epsilon) + 1 / (self.p * (math.exp(self.epsilon)) - 1)
+        y = (1.0 * self.numUsers) / (self.p * math.exp(self.epislon) - 1)
+        self.estimate = x * self.estimate - y
