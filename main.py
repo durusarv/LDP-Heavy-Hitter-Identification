70314985a4fc70340464d32248cde6f9699eab20
Jake Lipson <jake_lipson@yahoo.com>
2018-12-10 02:16:24 -0500

    this does not work

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import matplotlib.pyplot as plt
 from sample_selector import SampleSelector
 from optimized_local_hashing import OLH
 from generalized_random_response import GRR
+from prefix_extending_method import PEM
 
 def main():
-    domain = 32
-    sampleSize = 30
+    domain = 1024
+    sampleSize = 5000
     epsilon = 5
     ss = SampleSelector(sampleSize, domain)
     sampleSet = ss.sampleSet
-    real = getCounts(sampleSet, domain)
+    #real = getCounts(sampleSet, domain)
     grr = GRR(epsilon, domain, sampleSet)
     grrEstimate = grr.estimate
     print(sampleSet)
     print(real)
-
+    pemReal = PEM(sampleSet, domain)
     #olh = OLH(epsilon, domain, sampleSet)
     #olhEstimate = olh.estimate
     #print(olhEstimate)
--- prefix_extending_method.py
+++ prefix_extending_method.py
@@ -1,14 +1,55 @@
 import numpy as np
-               
+import random
+
+from generalized_random_response import GRR
+
 class PEM():
     def __init__(self, sampleSet, domain):
         self.sampleset = sampleSet
         self.domain = domain
         self.sampleSetBinary = np.copy(sampleSet)
+        self.m = self.getM(domain)
+        self.y = 8
+        self.orig_n = 4
+        self.n = self.orig_n
 
         for i in range(len(self.sampleSetBinary)):
-            sampleSetBinary[i] = intToBinaryString(sampleSetBinary[i], self.domain)
+            self.sampleSetBinary[i] = self.intToBinaryString(self.sampleSetBinary[i])
 
-def intToBinaryString(num, domain):
-    length = len("{0:b}".format(domain))
-    return bin(num)[2:].zfill(length)
+        self.g = (self.m - self.y) / self.n
+        self.G = [int(self.g)], []
+        #for array in self.G:
+        #    array.append()
+
+        self.putIntoGroups()
+        print(self.G)
+        self.prefixes = [], []
+        self.reportPrefixes();
+        #print(self.prefixes[len(self.prefixes) - 1])
+
+    def reportPrefixes(self):
+        for i in range(int(self.g)):
+            temp = []
+            for j in range(len(self.G[i])):
+                tempPrefix = intToBinaryString(self.G[i][j])[0:self.n]
+                if (not i == 0):
+                    for k in range(len(self.prefixes[i - 1])):
+                        if (tempPrefix[0:self.n - self.orig_n] == self.prefixes[i - 1][k]):
+                            temp.append(tempPrefix)
+            tempGRR = GRR(7, (self.y + ((i + 1) * self.orig_n), temp).perturbed
+            for num in tempGRR[0]:
+                num = intToBinaryString(num)
+            self.prefixes.append(tempGRR)
+            self.n += self.orig_n
+
+
+    def putIntoGroups(self):
+        for sample in self.sampleSet:
+            rand = random.random();
+            self.G[int(rand * self.g)].append(sample);
+
+    def intToBinaryString(self, num):
+        return (bin(int(num))[2:].zfill(self.domain))
+
+    def getM(self, domain):
+        return len("{0:b}".format(domain))
